508d10d9e39310f73df771c7da913e88ccd4f7db
Albert Sun <2692938841qq@gmail.com>
2026-03-12 20:40:04 -0600

    added a line that enables JSON parsing middleware

--- service/index.js
+++ service/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
