a41b55efb717586dde749d0d8b596f34a34fe5ff
Albert Sun <2692938841qq@gmail.com>
2026-03-12 20:46:39 -0600

    added in-memory users and sessions storage

--- service/index.js
+++ service/index.js
@@ -7,6 +7,9 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.json());
 app.use(cookieParser());
 
+const users = [];
+const sessions = {};
+
 app.get('/api/test', (req, res) => {
   res.send({ msg: 'startup service works' });
 });
