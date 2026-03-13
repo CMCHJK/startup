fbdf2019d1f7127f2309e67782207f3498133059
Albert Sun <2692938841qq@gmail.com>
2026-03-12 20:45:29 -0600

    add cookie parser midleware

--- service/index.js
+++ service/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/api/test', (req, res) => {
   res.send({ msg: 'startup service works' });
