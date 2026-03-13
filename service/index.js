316bded16bfb0134077881c35fce8a5425bc36b7
Albert Sun <2692938841qq@gmail.com>
2026-03-12 20:38:28 -0600

    created express server

---
+++ service/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
