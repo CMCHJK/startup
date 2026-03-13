244033ad516938a9f3682fb908dfbd636e4be413
Albert Sun <2692938841qq@gmail.com>
2026-03-12 20:42:32 -0600

    added test API endpoint

--- service/index.js
+++ service/index.js
@@ -5,6 +5,10 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
 
+app.get('/api/test', (req, res) => {
+  res.send({ msg: 'startup service works' });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
